b0bac93863e666294d535d1f267a771e657b5954
Laura Kirsch <laurakirsch240406@gmail.com>
2025-12-24 21:32:23 +0100

    more stuff

--- ruminant/tests.py
+++ ruminant/tests.py
@@ -27,7 +27,10 @@ def inv(func):
 
 
 def assert_eq(a, b):
-    assert a == b, f"Expected {b}, got {a}"
+    if isinstance(a, int) and isinstance(b, int):
+        assert a == b, f"Expected {b}, got {a} (i.e. {hex(a)})"
+    else:
+        assert a == b, f"Expected {b}, got {a}"
 
 
 @test("Sanity", "simple-pass")
@@ -58,432 +61,75 @@ def f():
     Buf("deadbeef")
 
 
-@test("Buffer", "ru8")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru8(), 0x81)
-
-
-@test("Buffer", "ru16")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru16(), 0x8182)
-
-
-@test("Buffer", "ru24")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru24(), 0x818283)
-
-
-@test("Buffer", "ru32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru32(), 0x81828384)
-
-
-@test("Buffer", "ru64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru64(), 0x8182838485868788)
-
-
-@test("Buffer", "ri8")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri8(), -0x7f)
-
-
-@test("Buffer", "ri16")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri16(), -0x7e7e)
-
-
-@test("Buffer", "ri24")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri24(), -0x7e7d7d)
-
-
-@test("Buffer", "ri32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri32(), -0x7e7d7c7c)
-
-
-@test("Buffer", "ri64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri64(), -0x7e7d7c7b7a797878)
-
-
-###
-@test("Buffer", "ru8l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru8l(), 0x81)
-
-
-@test("Buffer", "ru16l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru16l(), 0x8281)
-
-
-@test("Buffer", "ru24l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru24l(), 0x838281)
-
-
-@test("Buffer", "ru32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru32l(), 0x84838281)
-
-
-@test("Buffer", "ru64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru64l(), 0x8887868584838281)
-
-
-@test("Buffer", "ri8l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri8l(), -0x7f)
-
-
-@test("Buffer", "ri16l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri16l(), -0x7d7f)
-
-
-@test("Buffer", "ri24l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri24l(), -0x7c7d7f)
-
-
-@test("Buffer", "ri32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri32l(), -0x7b7c7d7f)
-
-
-@test("Buffer", "ri64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri64l(), -0x7778797a7b7c7d7f)
-
-
-@test("Buffer", "rf16")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf16(), -2.300739288330078e-05)
-
-
-@test("Buffer", "rf32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf32(), -4.794317366894298e-38)
-
-
-@test("Buffer", "rf64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf64(), -2.1597750994171683e-301)
-
-
-@test("Buffer", "rf16l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf16l(), -3.820657730102539e-05)
-
-
-@test("Buffer", "rf32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf32l(), -3.091780090135418e-36)
-
-
-@test("Buffer", "rf64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.rf64l(), -1.4249914579614907e-267)
-
-
-@test("Buffer", "rf32-inf")
-def f():
-    buf = Buf(bytes.fromhex("7f800000"))
-    assert_eq(buf.rf32(), float("inf"))
-
-
-@test("Buffer", "rf32-nan")
-def f():
-    buf = Buf(bytes.fromhex("7f800001"))
-    assert_eq(str(buf.rf32()), "nan")
-
-
-@test("Buffer", "rs")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.rs(3), "abc")
-    assert_eq(buf.tell(), 3)
-
-
-@test("Buffer", "rh")
-def f():
-    buf = Buf(bytes.fromhex("01020304"))
-    assert_eq(buf.rh(3), "010203")
-    assert_eq(buf.tell(), 3)
-
-
-@test("Buffer", "rzs")
-def f():
-    buf = Buf(b"abcd\x00efgh")
-    assert_eq(buf.rzs(), "abcd")
-    assert_eq(buf.tell(), 5)
-
-
-@test("Buffer", "rzs-with-end-as-zero")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.rzs(), "abcd")
-    assert_eq(buf.tell(), 5)
-
-
-@test("Buffer", "rl")
-def f():
-    buf = Buf(b"abcd\nefgh")
-    assert_eq(buf.rl(), b"abcd")
-    assert_eq(buf.tell(), 5)
-
-
-@test("Buffer", "rl-with-end-as-newline")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.rl(), b"abcd")
-    assert_eq(buf.tell(), 4)
-
-
-###
-
-
-@test("Buffer", "ru8")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru8(), 0x81)
-
-
-@test("Buffer", "ru16")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru16(), 0x8182)
-
-
-@test("Buffer", "ru24")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru24(), 0x818283)
-
-
-@test("Buffer", "ru32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru32(), 0x81828384)
-
-
-@test("Buffer", "ru64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ru64(), 0x8182838485868788)
-
-
-@test("Buffer", "ri8")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri8(), -0x7f)
-
-
-@test("Buffer", "ri16")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri16(), -0x7e7e)
-
-
-@test("Buffer", "ri24")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri24(), -0x7e7d7d)
-
-
-@test("Buffer", "ri32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri32(), -0x7e7d7c7c)
-
-
-@test("Buffer", "ri64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.ri64(), -0x7e7d7c7b7a797878)
-
-
-@test("Buffer", "pu8l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pu8l(), 0x81)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pu16l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pu16l(), 0x8281)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pu24l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pu24l(), 0x838281)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pu32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pu32l(), 0x84838281)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pu64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pu64l(), 0x8887868584838281)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pi8l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pi8l(), -0x7f)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pi16l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pi16l(), -0x7d7f)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pi24l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pi24l(), -0x7c7d7f)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pi32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pi32l(), -0x7b7c7d7f)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pi64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pi64l(), -0x7778797a7b7c7d7f)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf32")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pf32(), -4.794317366894298e-38)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf64")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pf64(), -2.1597750994171683e-301)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf32l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pf32l(), -3.091780090135418e-36)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf64l")
-def f():
-    buf = Buf(bytes.fromhex("8182838485868788"))
-    assert_eq(buf.pf64l(), -1.4249914579614907e-267)
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf32-inf")
-def f():
-    buf = Buf(bytes.fromhex("7f800000"))
-    assert_eq(buf.pf32(), float("inf"))
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pf32-nan")
-def f():
-    buf = Buf(bytes.fromhex("7f800001"))
-    assert_eq(str(buf.pf32()), "nan")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "ps")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.ps(3), "abc")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "ph")
-def f():
-    buf = Buf(bytes.fromhex("01020304"))
-    assert_eq(buf.ph(3), "010203")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pzs")
-def f():
-    buf = Buf(b"abcd\x00efgh")
-    assert_eq(buf.pzs(), "abcd")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pzs-with-end-as-zero")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.pzs(), "abcd")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pl")
-def f():
-    buf = Buf(b"abcd\nefgh")
-    assert_eq(buf.pl(), b"abcd")
-    assert_eq(buf.tell(), 0)
-
-
-@test("Buffer", "pl-with-end-as-newline")
-def f():
-    buf = Buf(b"abcd")
-    assert_eq(buf.pl(), b"abcd")
-    assert_eq(buf.tell(), 0)
+buffer_read_cases = [
+    (Buf.ru8, Buf.pu8, 0x81, 1),
+    (Buf.ru16, Buf.pu16, 0x8182, 2),
+    (Buf.ru24, Buf.pu24, 0x818283, 3),
+    (Buf.ru32, Buf.pu32, 0x81828384, 4),
+    (Buf.rf32, Buf.pf32, -4.794317366894298e-38, 4),
+    (Buf.ru64, Buf.pu64, 0x8182838485868788, 8),
+    (Buf.rf64, Buf.pf64, -2.1597750994171683e-301, 8),
+    (Buf.ri8, Buf.pi8, -0x7f, 1),
+    (Buf.ri16, Buf.pi16, -0x7e7e, 2),
+    (Buf.ri24, Buf.pi24, -0x7e7d7d, 3),
+    (Buf.ri32, Buf.pi32, -0x7e7d7c7c, 4),
+    (Buf.ri64, Buf.pi64, -0x7e7d7c7b7a797878, 8),
+    (Buf.ru8l, Buf.pu8l, 0x81, 1),
+    (Buf.ru16l, Buf.pu16l, 0x8281, 2),
+    (Buf.ru24l, Buf.pu24l, 0x838281, 3),
+    (Buf.ru32l, Buf.pu32l, 0x84838281, 4),
+    (Buf.rf32l, Buf.pf32l, -3.091780090135418e-36, 4),
+    (Buf.ru64l, Buf.pu64l, 0x8887868584838281, 8),
+    (Buf.rf64l, Buf.pf64l, -1.4249914579614907e-267, 8),
+    (Buf.ri8l, Buf.pi8l, -0x7f, 1),
+    (Buf.ri16l, Buf.pi16l, -0x7d7f, 2),
+    (Buf.ri24l, Buf.pi24l, -0x7c7d7f, 3),
+    (Buf.ri32l, Buf.pi32l, -0x7b7c7d7f, 4),
+    (Buf.ri64l, Buf.pi64l, -0x7778797a7b7c7d7f, 8),
+    (lambda buf: buf.rs(4), lambda buf: buf.ps(4), "abcd", 4, b"abcdefgh", "rs", "ps"),
+    (lambda buf: buf.rh(4), lambda buf: buf.ph(4), "81828384", 4, None, "rh", "ph"),
+    (Buf.rzs, Buf.pzs, "abcd", 5, b"abcd\x00efgh"),
+    (Buf.rzs, Buf.pzs, "abcd", 5, b"abcd", "rzs-end-as-zero", "pzs-end-as-zero"),
+    (Buf.rl, Buf.pl, b"abcd", 5, b"abcd\nefgh", "rl-lf", "pl-lf"),
+    (Buf.rl, Buf.pl, b"abcd", 6, b"abcd\r\nefgh", "rl-crlf", "pl-crlf"),
+    (Buf.rl, Buf.pl, b"abcd", 5, b"abcd\refgh", "rl-cr", "pl-cr"),
+    (Buf.rl, Buf.pl, b"abcd", 6, b"abcd\n\refgh", "rl-lfcr", "pl-lfcr"),
+    (Buf.rl, Buf.pl, b"abcd", 4, b"abcd", "rl-end", "pl-end"),
+    (Buf.rl, Buf.pl, b"abcd", 5, b"abcd\n", "rl-lfend", "pl-lfend"),
+    (Buf.rl, Buf.pl, b"abcd", 5, b"abcd\r", "rl-crend", "pl-crend"),
+    (
+        Buf.rguid,
+        Buf.pguid,
+        "64636261-6665-6867-696a-6b6c6d6e6f70",
+        16,
+        b"abcdefghijklmnop",
+    ),
+]
+
+
+def buf_instance(rf, pf, val, pos, buf_bytes=None, rname=None, pname=None):
+    if buf_bytes is None:
+        buf_bytes = bytes.fromhex("8182838485868788")
+    if rname is None:
+        rname = rf.__name__
+    if pname is None:
+        pname = pf.__name__
+
+    @test("Buffer", rname)
+    def f():
+        buf = Buf(buf_bytes)
+        assert_eq(rf(buf), val)
+        assert_eq(buf.tell(), pos)
+
+    @test("Buffer", pname)
+    def f():
+        buf = Buf(buf_bytes)
+        assert_eq(pf(buf), val)
+        assert_eq(buf.tell(), 0)
+
+
+for instance in buffer_read_cases:
+    buf_instance(*instance)
 
 
 @test("Buffer", "overread")
